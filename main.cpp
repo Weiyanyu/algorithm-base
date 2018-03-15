e2683f922218d347ec155da0a67b5f21a97a5beb
Weiyanyu <chinayeonon@gmail.com>
2018-03-15 14:09:46 +0800

    init project

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 72419 on 2018/3/15 0015.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+
+    string s = "Hello";
+    string s1 = " world";
+    string res = s + s1;
+
+    cout << res << endl;
+
+    return 0;
+}
+
